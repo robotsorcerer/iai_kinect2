770200c9bb512a570d024c844398d160ce7ee5a7
lakehanne <patlekano@gmail.com>
2015-09-16 11:32:24 -0500

    now under 1 sec

--- kinect2_viewer/src/viewer.cpp
+++ kinect2_viewer/src/viewer.cpp
@@ -14,13 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ #include <linux/stat.h>
+
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -59,7 +61,6 @@
 #include "savgol.h"
 #include <iostream>
 #include <cmath>
-
 
 using namespace std;
 using namespace cv;
